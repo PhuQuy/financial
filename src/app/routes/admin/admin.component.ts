842edf0f23cb3767ee53d4bfd4e197f9bcbcaf64
PhuQuy <phuquy.tool@gmail.com>
2018-12-12 14:39:28 +0700

    update

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MessagingService } from '@app/services/messaging.service';
 import { UserService } from '@app/services/user.service';
+import * as JsEncryptModule from 'jsencrypt';
+import { environment } from '@env/environment';
 
 @Component({
     selector: 'app-admin',
@@ -11,6 +13,7 @@ import { UserService } from '@app/services/user.service';
 export class AdminComponent implements OnInit {
     message;
     users = [];
+    encrypt;
     constructor(private messagingService: MessagingService, private userService: UserService) { }
 
     ngOnInit() {
@@ -19,7 +22,20 @@ export class AdminComponent implements OnInit {
         this.message = this.messagingService.currentMessage;
         this.userService.getAlls().subscribe(users => {
             this.users = users;
-        })
+        });
+
+        // this.users.map((item, index) => {
+        //     item.phone = this.encrypt.decrypt(item.phone);
+        // })
+
+        this.encrypt = new JsEncryptModule.JSEncrypt();
+        this.encrypt.setPublicKey(environment.publishSSHRASKey);
+        var encrypted = this.encrypt.encrypt('phuquy');
+
+        console.log(encrypted);
+        this.encrypt.setPrivateKey(environment.privateSSHRASKey);
+        var uncrypted = this.encrypt.decrypt(encrypted);
+        console.log(uncrypted);
     }
 
 }
