f1e37e423f86175165d0f8bce0f2d47bfc710124
PhuQuy <phuquy.tool@gmail.com>
2018-12-17 10:24:10 +0700

    update modals

--- src/app/layouts/client/client.module.ts
+++ src/app/layouts/client/client.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CustomerFeedbackComponent } from '@app/components/customer-feedback/customer-feedback.component';
-import { FooterComponent } from '@app/components/footer/footer.component';
-import { NavbarComponent } from '@app/components/navbar/navbar.component';
-import { NewsItemModule } from '@app/components/news-item/news-item.module';
+import { CustomerFeedbackComponent } from '@components/customer-feedback/customer-feedback.component';
+import { FooterComponent } from '@components/footer/footer.component';
+import { NavbarComponent } from '@components/navbar/navbar.component';
+import { NewsItemModule } from '@components/news-item/news-item.module';
 import { ContactUsComponent } from '@app/routes/contact-us/contact-us.component';
 import { HomeComponent } from '@app/routes/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
