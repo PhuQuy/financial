e6680e3c8d44ebfa8843cb5f66ec79a61198c334
PhuQuy <phuquy.tool@gmail.com>
2019-01-15 08:24:50 +0700

    update user management

--- src/app/routes/admin/user/user-detail/user-detail.component.ts
+++ src/app/routes/admin/user/user-detail/user-detail.component.ts
@@ -5,41 +5,58 @@ import * as JsEncryptModule from 'jsencrypt';
 import { environment } from '@env/environment';
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss'],
-  providers: [UserService]
+    selector: 'app-user-detail',
+    templateUrl: './user-detail.component.html',
+    styleUrls: ['./user-detail.component.scss'],
+    providers: [UserService]
 })
 export class UserDetailComponent implements OnInit {
-  user:any = new Object();
-  id: any;
-  alls: any;
-  encrypt;
-
-  constructor(
-    private userService: UserService,
-    private route: ActivatedRoute
-  ) {
-    this.encrypt = new JsEncryptModule.JSEncrypt();
-    this.encrypt.setPrivateKey(environment.privateSSHRASKey);
-    this.route.params.subscribe(params => {
-      this.id = params['id'];
-      
-    });
-    this.alls = this.userService.getById(this.id);
-  }
-
-  ngOnInit() {
-    if (this.alls) {
-      this.alls.subscribe(user => {
-          this.user = user;
-          this.user.phone = this.encrypt.decrypt(this.user.phone);
-      });
-      
+    user: any = new Object();
+    id: any;
+    alls: any;
+    encrypt;
+    breadcrumbs;
+    basicDisabled = true;
+    constructor(
+        private userService: UserService,
+        private route: ActivatedRoute
+    ) {
+        this.encrypt = new JsEncryptModule.JSEncrypt();
+        this.encrypt.setPrivateKey(environment.privateSSHRASKey);
+        this.route.params.subscribe(params => {
+            this.id = params['id'];
+        });
+        this.alls = this.userService.getById(this.id);
     }
-    
-    
 
-}
+    ngOnInit() {
+        if (this.alls) {
+            this.alls.subscribe(user => {
+                this.user = user;
+                console.log(this.user);
+
+                this.user.phone = this.encrypt.decrypt(this.user.phone);
+                this.breadcrumbs = [
+                    {
+                        router: '/admin',
+                        title: 'Home'
+                    },
+                    {
+                        router: '/admin/users',
+                        title: 'Users'
+                    },
+                    {
+                        router: '/admin/users',
+                        title: user.name
+                    }
+                ]
+            });
+
+        }
+    }
+
+    basicEdit() {
+        this.basicDisabled = false;
+    }
 
 }
