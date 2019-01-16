c5435e3ce7d836b7837337d652ec2f53a30c058b
Erik Lee <lyhuynhduybao@gmail.com>
2019-01-16 11:35:05 +0700

    Merge

--- src/app/routes/log-in/log-in.module.ts
+++ src/app/routes/log-in/log-in.module.ts
@@ -4,7 +4,8 @@ import { LogInComponent } from './log-in.component';
 import { LoginRoutingModule } from './log-in.routing';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { StoreModule } from '@ngrx/store';
+import { authReducer } from '@app/components/redux/reducers/auth.reducer';
 
 @NgModule({
   imports: [
